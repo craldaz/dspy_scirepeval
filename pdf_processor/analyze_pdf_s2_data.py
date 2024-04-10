cf4f01436e552262f780551dd8d444d8febf8923
Cody Aldaz <cody@acelot.com>
2024-04-09 18:27:38 -0700

    👌 IMPROVE: some more fixes

--- pdf_processor/analyze_pdf_s2_data.py
+++ pdf_processor/analyze_pdf_s2_data.py
@@ -7,6 +7,9 @@ Also, the classification of the papers is used to find the classification of the
 query_papers: The papers that are used as queries
 candidate_papers: The papers that are used as candidates
 retrieved_papers: The papers that are retrieved by the candidate papers
+
+link-recorder-final-1: The data that contains the relationships between candidate papers and retrieved papers
+test.qrel.cid: The data that contains the test relationships between query papers and candidate papers
 classification_meta: The classification of the papers in the test set
 '''
 import pandas as pd
@@ -21,7 +24,6 @@ query_candidate_data = pd.read_csv('darwin/test.qrel.cid', sep=' ', header=None,
 # Load the classification meta data
 with open('darwin/classification_meta.jsonl') as f:
     classification_meta = [json.loads(line) for line in f]
-
 
 valid_rows = pd.DataFrame()
 query_dir = 'darwin/query_papers'
@@ -49,11 +51,14 @@ valid_rows.to_csv('darwin/valid_query_candidate_pairs.csv', index=False)
 classification_labels=[(0, 'Agricultural and Food sciences'), (1, 'Art'), (2, 'Biology'), (3, 'Business'), (4, 'Chemistry'), (5, 'Computer science'), (6, 'Economics'), (7, 'Education'), (8, 'Engineering'), (9, 'Environmental science'), (11, 'Geology'), (12, 'History'), (13, 'Law'), (14, 'Linguistics'), (15, 'Materials science'), (16, 'Mathematics'), (17, 'Medicine'), (18, 'Philosophy'), (19, 'Physics'), (20, 'Political science'), (21, 'Psychology'), (22, 'Sociology')]
 
 # Go through each valid query paper and find the classification
+found_classifications = []
+print("Processing query papers and their candidate papers classification")
 for query_paper in unique_query_papers:
-    print(f'Processing query paper: {query_paper}')
+    # print(f'Processing query paper: {query_paper} for classification task')
     # Find the classification of the query paper
-    query_classification = [meta['classification'] for meta in classification_meta if meta['corpus_id'] == query_paper]
+    query_classification = [meta['labels'] for meta in classification_meta if meta['corpus_id'] == query_paper]
     if len(query_classification) != 0:
+        print('Processing query paper: {query_paper} for classification task')
         print(f'{query_paper=}, {query_classification=}')
 
         # Find the candidate papers that are related to the query paper
@@ -62,19 +67,19 @@ for query_paper in unique_query_papers:
 
         # Find the classification of each candidate paper
         for candidate_paper in related_candidates:
-            classification = [meta['classification'] for meta in classification_meta if meta['corpus_id'] == candidate_paper]
+            classification = [meta['labels'] for meta in classification_meta if meta['corpus_id'] == candidate_paper]
             if len(classification) != 0:
                 print(f'{candidate_paper=}, {classification=}')
-
+                found_classifications.append((query_classification[0], classification[0]))
 
 # Create a list of unique paper IDs in numerical order
 candidate_paper_ids = sorted(set(candidate_retrieved_data['candidate_paper'].tolist()))
 retrieved_paper_ids = sorted(set(candidate_retrieved_data['retrieved_paper'].tolist()))
 
+
 # Create a dictionary that maps paper IDs to matrix indices
 candidate_id_to_index = {paper_id: index for index, paper_id in enumerate(candidate_paper_ids)}
 retrieved_id_to_index = {paper_id: index for index, paper_id in enumerate(retrieved_paper_ids)}
-
 
 # Create an empty matrix
 matrix = np.zeros((len(candidate_id_to_index), len(retrieved_id_to_index)))
@@ -109,3 +114,20 @@ print(f'\tmean: {matrix_df[matrix_df.sum(axis=1) > 0].sum(axis=1).mean()}')
 print(f'\tstd: {matrix_df[matrix_df.sum(axis=1) > 0].sum(axis=1).std()}')
 print(f'\tmin: {matrix_df[matrix_df.sum(axis=1) > 0].sum(axis=1).min()}')
 print(f'\tmax: {matrix_df[matrix_df.sum(axis=1) > 0].sum(axis=1).max()}')
+
+
+found_classification_candidate = [] 
+print('Processing candidate papers and their retrieved papers classification')
+for candidate_paper in candidate_paper_ids:
+    classification = [meta['labels'] for meta in classification_meta if meta['corpus_id'] == candidate_paper]
+    if len(classification) != 0:
+        print(f'{candidate_paper=}, {classification=}')
+        found_classification_candidate.append(('unknown', classification[0]))
+
+        retrieved_paper_for_candidate = matrix_df.loc[candidate_paper][matrix_df.loc[candidate_paper] == 1].index.tolist()
+        print(f'{retrieved_paper_for_candidate=}')
+        for retrieved_paper in retrieved_paper_for_candidate:
+            classification = [meta['labels'] for meta in classification_meta if meta['corpus_id'] == retrieved_paper]
+            if len(classification) != 0:
+                print(f'{retrieved_paper=}, {classification=}')
+                found_classification_candidate.append(('unknown', classification[0]))
